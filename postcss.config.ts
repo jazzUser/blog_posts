cb7c0424ab6e69151b294f2fd4d66de25bb74dff
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-12-19 15:33:59 +0100

    initial commit

---
+++ postcss.config.ts
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {}, // Tells PostCSS to process Tailwind CSS
+      autoprefixer: {}, // Tells PostCSS to automatically add vendor prefixes
+    },
+  };
+  
